d4eed12c1f8fe72a4eabff1908213d37f484618a
Rohan859 <93860329+Rohan859@users.noreply.github.com>
2024-05-23 23:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const list=document.getElementById("infi-list");
 
+function addListItem(count)
+{
+	for (let i = 0; i <count; i++) 
+	{
+		const listItem=document.getElementById("li");
+		listItem.textContent=`item${list.children.length+1}`;
+		list.appendChild(listItem);
+	}
+}
+
+addListItem(10);
+
+list.addEventListener("scroll",()=>{
+	if(this.scrollTop+this.clientHeight>=this.scrollHeight)
+	{
+		addListItem(2);
+	}
+})
